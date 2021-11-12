5e1bbe78125794779bb6f3ae5bcf50be2493a7ce
prantobiswas321 <rahulbd321@gmail.com>
2021-11-12 16:06:36 +0600

    admin check api done

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
 
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
 
         // get my orders
